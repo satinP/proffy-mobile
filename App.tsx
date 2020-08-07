67501f0603b391977336cc795faa316d99bdfb13
Pedro Satin <pedro5satin@gmail.com>
2020-08-06 22:20:27 -0300

    First Commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import Landing from './src/pages/Landing';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
+      <Landing />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
---
+++ src/pages/Landing/styles.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#8257E5',
+    flex: 1,
+    justifyContent: 'center',
+    padding: 40,
+  },
+  banner: {
+    width: '100%',
+    resizeMode: 'contain',
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 20,
+    lineHeight: 30,
+    marginTop: 80,
+  },
+  titleBold: {
+    fontWeight: 'bold',
+  }
+
+});
+
+export default styles;
